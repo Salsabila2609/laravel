354b9bb1f28c536c795c8518e69f4d01d7caa7d8
Apple <mutiaranurhaliza24@gmail.com>
2025-02-13 11:48:01 +0700

    pindah dashboard

--- routes/web.php
+++ routes/web.php
@@ -26,10 +26,6 @@ use App\Http\Controllers\PopupController;
 use App\Http\Controllers\MediaController;
 
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
