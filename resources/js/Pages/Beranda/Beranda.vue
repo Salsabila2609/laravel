e357c6c921a314dd7102b2f2d8cb3d58e32c0bf8
Salsabila2609 <128382995+Salsabila2609@users.noreply.github.com>
2025-02-19 17:06:24 +0700

    Secure Beranda.vue

--- resources/js/Pages/Beranda/Beranda.vue
+++ resources/js/Pages/Beranda/Beranda.vue
@@ -1,38 +1,37 @@
 <template>
   <div>
     <MasterLayout>
+      <!-- Video Section -->
+      <Video v-if="isValidUrl(latestVideo)" :videoSource="latestVideo" />
+      
+      <!-- Media Section -->
+      <Media v-if="isValidArray(media)" :media="media" />
+      
+      <!-- Berita Section -->
+      <Berita v-if="isValidObject(mainNews) && isValidArray(newsCards)"
+        :mainNews="mainNews"
+        :newsCards="newsCards"
+      />
+      
+      <!-- Popup Section -->
+      <Popup v-if="showPopup && isValidObject(popup)" :popup="popup" />
 
-    <!-- Video Section -->
-    <Video :videoSource="latestVideo" /> 
-    <!-- Media Section -->
-    <Media :media="media" />
-    <!-- Berita Section -->
-    <Berita
-      :mainNews="mainNews"
-      :newsCards="newsCards"
-    />
-    <!-- Popup Section -->
-    <Popup v-if="showPopup" :popup="popup" />
-
-    <!-- Visitor Section -->
-    <Layanan :layanans="layanans" />
-    <VideoGallery />
-    <Visitor
-      :visitorCount="visitorCount"
-      :todayVisitorCount="todayVisitorCount"
-      :monthlyVisitorCount="monthlyVisitorCount"
-      :yearlyVisitorCount="yearlyVisitorCount"
-    />
-
+      <!-- Visitor Section -->
+      <Layanan v-if="isValidArray(layanans)" :layanans="layanans" />
+      <VideoGallery />
+      <Visitor v-if="isValidNumber(visitorCount) && isValidNumber(todayVisitorCount) && isValidNumber(monthlyVisitorCount) && isValidNumber(yearlyVisitorCount)"
+        :visitorCount="visitorCount"
+        :todayVisitorCount="todayVisitorCount"
+        :monthlyVisitorCount="monthlyVisitorCount"
+        :yearlyVisitorCount="yearlyVisitorCount"
+      />
     </MasterLayout>
-   
   </div>
 </template>
 
 <script>
 import MasterLayout from '@/Layouts/MasterLayout.vue';
-
-import Video from '@/Components/Layout/Beranda/VideoSection.vue'; // Import Video.vue
+import Video from '@/Components/Layout/Beranda/VideoSection.vue';
 import Visitor from "@/Components/Layout/Beranda/Visitor.vue";
 import Berita from "@/Components/Layout/Beranda/Berita.vue";
 import Layanan from "@/Components/Layout/Beranda/Layanan.vue";
@@ -42,11 +41,9 @@ import Media from '@/Components/Layout/Beranda/Media.vue';
 
 export default {
   components: {
-
-    Video, // Tambahkan Video.vue
+    Video,
     Visitor,
     MasterLayout,
-
     Berita,
     Layanan,
     VideoGallery,
@@ -54,21 +51,35 @@ export default {
     Media,
   },
   props: {
-    latestVideo: String, // Tambahkan prop ini
-    media: Array,
-    visitorCount: Number,
-    todayVisitorCount: Number,
-    monthlyVisitorCount: Number,
-    yearlyVisitorCount: Number,
-    mainNews: Object,
-    newsCards: Array,
-    popup: Object,
-    layanans: Array,
+    latestVideo: { type: String, default: '' },
+    media: { type: Array, default: () => [] },
+    visitorCount: { type: Number, default: 0 },
+    todayVisitorCount: { type: Number, default: 0 },
+    monthlyVisitorCount: { type: Number, default: 0 },
+    yearlyVisitorCount: { type: Number, default: 0 },
+    mainNews: { type: Object, default: () => ({}) },
+    newsCards: { type: Array, default: () => [] },
+    popup: { type: Object, default: () => ({}) },
+    layanans: { type: Array, default: () => [] },
   },
   data() {
     return {
       showPopup: true,
     };
   },
+  methods: {
+    isValidUrl(url) {
+      return typeof url === 'string' && /^(https?:\/\/|\/storage\/).+\.(mp4|webm|mov)$/i.test(url);
+    },
+    isValidArray(arr) {
+      return Array.isArray(arr) && arr.length > 0;
+    },
+    isValidObject(obj) {
+      return obj !== null && typeof obj === 'object' && Object.keys(obj).length > 0;
+    },
+    isValidNumber(num) {
+      return typeof num === 'number' && num >= 0;
+    }
+  }
 };
 </script>
